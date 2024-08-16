bb9bee985fa643b0f2b35d5625c6940337c97886
dotuankiet2302 <dotuankiet2302@gmail.com>
2024-08-16 16:51:11 +0700

    upload category

---
+++ Category_Task1/Category_Task1/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Category_Task1.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Category_Task1.Data
+{
+    //Install package Microsoft.EntityFrameworkCore
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Category_Task1/Category_Task1/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Category_Task1.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+    }
+}
